e2ff0a68e55d959978f2d86cfb6afe519b666a99
pierrecabriere <hello@pierrecabriere.fre>
2020-10-01 20:49:51 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,3 +1,54 @@
 # rxjs-auth
 
 Simple auth-manager based on rxjs
+
+
+--
+
+## Usage
+
+### Create manager
+
+```js
+import RxjsAuth from "rxjs-auth";
+
+const authmanager = RxjsAuth.create("myProjectIdentifier", {
+  fetchToken: (credentials) => axios.post("/give-me-my-token", credentials).then(res => res.data),
+  fetchUser: (token) => axios.post("/who-am-i", { headers: { "Authorization": "Bearer " + token } }).then(res => res.data),
+  // optional
+  isUserLogged: (resFromFetchUser) => !!resFromFetchUser
+});
+
+export { authmanager };
+```
+
+### Enjoy !
+
+```js
+import { authmanager } from "./path/to/manager";
+
+console.log(authmanager.logged); // false
+console.log(authmanager.user); // null
+await authmanager.login(credentials);
+console.log(authmanager.logged); // true
+console.log(authmanager.user); // ...
+```
+
+### Subscribe
+
+```js
+import { authmanager } from "./path/to/manager";
+
+authmanager.loadingSubject.subscribe(_loading => console.log("loading: " + _loading));
+authmanager.loggedSubject.subscribe(_logged => console.log("logged: " + _logged));
+authmanager.userSubject.subscribe(_user => console.log("user: " + _user));
+```
+
+### Sync at startup
+
+```js
+import { authmanager } from "./path/to/manager";
+
+// Fetch the user from the previously stored token
+authmanager.sync();
+```
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,111 @@
-export * from './lib/number';
+import { BehaviorSubject } from 'rxjs';
+
+interface IAuthOptions {
+  fetchToken?,
+  fetchUser?,
+  isUserLogged?
+}
+
+const defaultOptions: IAuthOptions = {
+  isUserLogged: data => !!data && Object.keys(data).length > 0
+}
+
+class Auth {
+  name;
+  options;
+  loadingSubject = new BehaviorSubject(false);
+  loggedSubject = new BehaviorSubject(false);
+  userSubject = new BehaviorSubject(null);
+
+  constructor(name: string, options: IAuthOptions) {
+    this.name = name;
+    this.options = Object.assign({}, defaultOptions, options);
+  }
+
+  static create(name: string, options: IAuthOptions) {
+    return new Auth(name, options);
+  }
+
+  get loading() {
+    return this.loadingSubject.getValue();
+  }
+
+  get logged() {
+    return this.loggedSubject.getValue();
+  }
+
+  get user() {
+    return this.userSubject.getValue();
+  }
+
+  getTokenName() {
+    return this.name ? `rxjs-auth_${ this.name }` : "rxjs-auth";
+  }
+
+  getToken() {
+    return localStorage.getItem(this.getTokenName());
+  }
+
+  setToken(token) {
+    localStorage.setItem(this.getTokenName(), token);
+    return this;
+  }
+
+  deleteToken() {
+    localStorage.removeItem(this.getTokenName());
+    return this;
+  }
+
+  fetchToken(...args) {
+    const token = this.options.fetchToken?.call(this, ...args);
+    this.setToken(token);
+    return token;
+  }
+
+  fetchUser(...args) {
+    const token = this.getToken();
+    return this.options.fetchUser?.call(this, token, ...args);
+  }
+
+  async login(...args) {
+    this.loadingSubject.next(true);
+    try {
+      await this.fetchToken(...args);
+      const user = await this.fetchUser();
+      const isLogged = await this.options.isUserLogged(user);
+      if (isLogged) {
+        this.userSubject.next(user);
+        this.loggedSubject.next(true);
+      }
+      this.loadingSubject.next(false);
+    } catch (e) {
+      this.loadingSubject.next(false);
+      throw e;
+    }
+  }
+
+  async sync() {
+    this.loadingSubject.next(true);
+    try {
+      const user = await this.fetchUser();
+      const isLogged = await this.options.isUserLogged(user);
+      if (isLogged) {
+        this.userSubject.next(user);
+        this.loggedSubject.next(true);
+      }
+      this.loadingSubject.next(false);
+    } catch (e) {
+      this.loadingSubject.next(false);
+      throw e;
+    }
+  }
+
+  logout() {
+    this.deleteToken();
+    this.loggedSubject.next(false);
+    this.userSubject.next(false);
+    return this;
+  }
+}
+
+export default Auth;
